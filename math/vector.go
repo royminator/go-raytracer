540247662d9122912b662802e011819213656f3a
Roy <roy.angelsen@gmail.com>
2023-12-13 23:16:49 +0100

    feat: add, sub, mul, div, negate, magnitude

--- math/vector.go
+++ math/vector.go
@@ -1,7 +1,15 @@
 package math
 
+import (
+	"math"
+)
+
 type (
 	Vec4 struct { X, Y, Z, W float64 }
+)
+
+const (
+	EPSILON = 0.0001
 )
 
 func Point4(x, y, z float64) Vec4 {
@@ -11,3 +19,39 @@ func Point4(x, y, z float64) Vec4 {
 func Vector4(x, y, z float64) Vec4 {
 	return Vec4{x, y, z, 0.0}
 }
+
+func Vec4With(v float64) Vec4 {
+	return Vec4{v, v, v, v}
+}
+
+func (lhs *Vec4) Add(rhs Vec4) Vec4 {
+	return Vec4{lhs.X+rhs.X, lhs.Y+rhs.Y, lhs.Z+rhs.Z, lhs.W+rhs.W}
+}
+
+func (lhs *Vec4) Sub(rhs Vec4) Vec4 {
+	return Vec4{lhs.X-rhs.X, lhs.Y-rhs.Y, lhs.Z-rhs.Z, lhs.W-rhs.W}
+}
+
+func (v *Vec4) Mul(x float64) Vec4 {
+	return Vec4{v.X*x, v.Y*x, v.Z*x, v.W*x}
+}
+
+func (v *Vec4) Div(x float64) Vec4 {
+	return Vec4{v.X/x, v.Y/x, v.Z/x, v.W/x}
+}
+
+func (v *Vec4) Negate() Vec4 {
+	return v.Mul(-1)
+}
+
+func (v *Vec4) Magnitude() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y + v.Z*v.Z + v.W*v.W)
+}
+
+func eqApprox(a, b float64) bool {
+	if math.Abs(a - b) < EPSILON {
+		return true
+	}
+	return false
+}
+
--- math/vector_test.go
+++ math/vector_test.go
@@ -1,18 +1,102 @@
 package math
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func TestCreatPointWShouldBe1(t *testing.T) {
 	actual := Point4(4, -4, 3)
 	expected := Vec4{4, -4, 3, 1}
-	if expected != actual {
-		t.Errorf("Expected %v, got %v", expected, actual)
-	}
+	checkResult(expected, actual, t)
 }
 
 func TestCreateVectorWShouldBe0(t *testing.T) {
 	actual := Vector4(4, -4, 3)
 	expected := Vec4{4, -4, 3, 0}
+	checkResult(expected, actual, t)
+}
+
+func TestAddVec4(t *testing.T) {
+	a1 := Vec4{3, -2, 5, 1}
+	a2 := Vec4{-2, 3, 1, 0}
+	expected := Vec4{1, 1, 6, 1}
+	actual := a1.Add(a2)
+	checkResult(expected, actual, t)
+}
+
+func TestSubPoints(t *testing.T) {
+	a1 := Point4(3, 2, 1)
+	a2 := Point4(5, 6, 7)
+	expected := Vector4(-2, -4, -6)
+	actual := a1.Sub(a2)
+	checkResult(expected, actual, t)
+}
+
+func TestSubVecFromPoint(t *testing.T) {
+	a1 := Point4(3, 2, 1)
+	a2 := Vector4(5, 6, 7)
+	expected := Point4(-2, -4, -6)
+	actual := a1.Sub(a2)
+	checkResult(expected, actual, t)
+}
+
+func TestSubVectors(t *testing.T) {
+	a1 := Vector4(3, 2, 1)
+	a2 := Vector4(5, 6, 7)
+	expected := Vector4(-2, -4, -6)
+	actual := a1.Sub(a2)
+	checkResult(expected, actual, t)
+}
+
+func TestSubFromZeroVec(t *testing.T) {
+	zero := Vec4With(0)
+	v := Vector4(1, -2, 3)
+	expected := Vector4(-1, 2, -3)
+	actual := zero.Sub(v)
+	checkResult(expected, actual, t)
+}
+
+func TestNegate(t *testing.T) {
+	expected := Vec4{1, -2, 3, -4}
+	actual := (&Vec4{-1, 2, -3, 4}).Negate()
+	checkResult(expected, actual, t)
+}
+
+func TestScalarMul(t *testing.T) {
+	expected := Vec4{3.5, -7, 10.5, -14}
+	actual := (&Vec4{1, -2, 3, -4}).Mul(3.5)
+	checkResult(expected, actual, t)
+}
+
+func TestScalarMulFrac(t *testing.T) {
+	expected := Vec4{0.5, -1, 1.5, -2}
+	actual := (&Vec4{1, -2, 3, -4}).Mul(0.5)
+	checkResult(expected, actual, t)
+}
+
+func TestScalarDiv(t *testing.T) {
+	expected := Vec4{0.5, -1, 1.5, -2}
+	actual := (&Vec4{1, -2, 3, -4}).Div(2)
+	checkResult(expected, actual, t)
+}
+
+func TestMagn(t *testing.T) {
+	type testCase struct {vec Vec4; res float64}
+	cases := []testCase{
+		{Vector4(1, 0, 0), 1},
+		{Vector4(0, 1, 0), 1},
+		{Vector4(0, 0, 1), 1},
+		{Vector4(1, 2, 3), math.Sqrt(14)},
+		{Vector4(-1, -2, -3), math.Sqrt(14)},
+	}
+	for _, c := range cases {
+		checkResult(c.res, c.vec.Magnitude(), t)
+	}
+}
+
+/////////////////////// HELPERS /////////////////////// 
+func checkResult(expected, actual interface{}, t *testing.T) {
 	if expected != actual {
 		t.Errorf("Expected %v, got %v", expected, actual)
 	}
