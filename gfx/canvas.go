2ab80429bba4b333d19efeb3c342e9d5e5230550
Roy Angelsen <roy.angelsen@gmail.com>
2023-12-18 13:22:20 +0100

    feat: write ppm

--- gfx/canvas.go
+++ gfx/canvas.go
@@ -1,6 +1,11 @@
 package gfx
 
-import m "raytracer/math"
+import (
+	"fmt"
+	"math"
+
+	m "roytracer/math"
+)
 
 type (
 	Canvas struct {
@@ -14,27 +19,78 @@ type (
 	}
 )
 
+const (
+	PPMFormat   = "P3"
+	PPMMaxColor = 255
+	PPMMaxCharsPerLine = 70
+)
+
 var (
 	ColorWhite m.Vec4 = m.Vec4{1, 1, 1, 0}
+	ColorBlack m.Vec4 = m.Vec4{0, 0, 0, 0}
 	ColorRed   m.Vec4 = m.Vec4{1, 0, 0, 0}
 )
 
-func NewCanvas(width, height uint32, color m.Vec4) Canvas {
-	pixels := make([][]m.Vec4, width)
+func NewCanvas(width, height uint32, color m.Vec4) *Canvas {
+	pixels := make([][]m.Vec4, height)
 	for i := range pixels {
-		col := make([]m.Vec4, height)
-		for j := range col {
-			col[j] = color
+		row := make([]m.Vec4, width)
+		for n := range row {
+			row[n] = color
 		}
-		pixels[i] = col
+		pixels[i] = row
 	}
-	return Canvas{
+	return &Canvas{
 		Width:  width,
 		Height: height,
 		Pixels: pixels,
 	}
 }
 
-func (c *Canvas) WritePixel(i, j uint32, color m.Vec4) {
-	c.Pixels[i][j] = color
+func (c *Canvas) WritePixel(n, m uint32, color m.Vec4) {
+	c.Pixels[m][n] = color
+}
+
+func (c *Canvas) CreatePPMHeader() string {
+	return fmt.Sprintf("%s\n%d %d\n%d\n", PPMFormat, c.Width, c.Height, PPMMaxColor)
+}
+
+func (c *Canvas) CreatePPMPixelData() string {
+	var pixelData string
+	for m := uint32(0); m < c.Height; m++ {
+		var line string
+		for n := uint32(0); n < c.Width; n++ {
+			colorString := colorToPPM(c.Pixels[m][n], PPMMaxColor)
+			line += colorString
+			if n != c.Width-1 {
+				line += " "
+			}
+		}
+		pixelData += line
+		pixelData += "\n"
+	}
+	return pixelData
+}
+
+func (c *Canvas) ToPPM() string {
+	ppm := c.CreatePPMHeader()
+	ppm += c.CreatePPMPixelData()
+	return ppm
+}
+
+func clamp(color float64, maxVal float64) float64 {
+	if color > 1 {
+		return maxVal
+	}
+	if color < 0 {
+		return 0
+	}
+	return color*maxVal
+}
+func colorToPPM(color m.Vec4, maxVal int) string {
+	return fmt.Sprintf("%d %d %d",
+		int(math.Round(clamp(color[0], PPMMaxColor))),
+		int(math.Round(clamp(color[1], PPMMaxColor))),
+		int(math.Round(clamp(color[2], PPMMaxColor))),
+	)
 }
--- gfx/canvas_test.go
+++ gfx/canvas_test.go
@@ -1,32 +1,74 @@
 package gfx
 
-import "testing"
+import (
+	"strings"
+	"testing"
 
-func TestNewCanvasIsAllWhite(t *testing.T) {
-	c := NewCanvas(10, 20, ColorWhite)
+	m "roytracer/math"
+)
+
+func TestNewCanvasIsAllBlack(t *testing.T) {
+	c := NewCanvas(10, 20, ColorBlack)
 
 	assertEqual(uint32(10), c.Width, t)
 	assertEqual(uint32(20), c.Height, t)
 
 	for i := range c.Pixels {
 		for j := range c.Pixels[i] {
-			assertEqual(c.Pixels[i][j], ColorWhite, t)
+			assertEqual(c.Pixels[i][j], ColorBlack, t)
 		}
 	}
 }
 
 func TestCanvasWritePixel(t *testing.T) {
-	c := NewCanvas(10, 20, ColorWhite)
+	c := NewCanvas(10, 20, ColorBlack)
 	var i, j uint32 = 2, 3
-	c.WritePixel(i, j, ColorRed)
+	c.WritePixel(j, i, ColorRed)
 	expected := ColorRed
 	actual := c.Pixels[i][j]
 	assertEqual(expected, actual, t)
 }
 
-/////////////////////// HELPERS /////////////////////// 
+func TestCanvasPPMHeader(t *testing.T) {
+	expected := lines("P3\n5 3\n255\n", 0, 2)
+	actual := lines(NewCanvas(5, 3, ColorBlack).ToPPM(), 0, 2)
+	if expected != actual {
+		t.Errorf("Expected %s, got %s", expected, actual)
+	}
+}
+
+func TestCanvasPPMPixelData(t *testing.T) {
+	expected := `255 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 128 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0 0 0 0 0 255`
+
+	c := NewCanvas(5, 3, ColorBlack)
+	c.WritePixel(0, 0, m.Color4(1.5, 0, 0, 0))
+	c.WritePixel(2, 1, m.Color4(0, 0.5, 0, 0))
+	c.WritePixel(4, 2, m.Color4(-0.5, 0, 1, 0))
+
+	actual := lines(c.ToPPM(), 3, 6)
+
+	if expected != actual {
+		t.Errorf("Expected %s, got %s", []byte(expected), []byte(actual))
+	}
+}
+
+// ///////////////////// HELPERS ///////////////////////
 func assertEqual(expected, actual interface{}, t *testing.T) {
 	if expected != actual {
 		t.Errorf("Expected %v, got %v", expected, actual)
 	}
+}
+
+func lines(text string, from, to int) string {
+	ls := strings.Split(text, "\n")
+	var res string
+	for i := from; i < to; i++ {
+		res += ls[i]
+		if i != to - 1 {
+			res += "\n"
+		}
+	}
+	return res
 }
